393d5d1ef55bfef579b13e120b66002ca1168719
Michael Jackson <mj@mjackson.me>
2018-12-30 07:07:24 -0400

    added support to APP_SRC environment variable and src by default to relative imports fro source code

--- packages/react-scripts/scripts/utils/verifyTypeScriptSetup.js
+++ packages/react-scripts/scripts/utils/verifyTypeScriptSetup.js
@@ -124,8 +124,9 @@ function verifyTypeScriptSetup() {
     // We do not support absolute imports, though this may come as a future
     // enhancement
     baseUrl: {
-      value: undefined,
-      reason: 'absolute imports are not supported (yet)',
+      value: process.env.APP_SRC,
+      reason:
+        'support relative imports from base source with APP_SRC=src by default',
     },
     paths: { value: undefined, reason: 'aliased imports are not supported' },
   };
